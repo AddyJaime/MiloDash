a8425598e20e4d58f9752fc31b3dfba9b8992518
addy <addyjaime@gmail.com>
2025-09-09 19:02:47 -0400

    added serializer field and if stamtent

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -1,18 +1,36 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Driver : MonoBehaviour
 {
 
     [SerializeField] float steerSpeed = .5f;
-    [SerializeField] float moveSpeed = .03f;    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] float moveSpeed = .03f;  
     void Update()
     {
+
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            Debug.Log("I am going forward");
+        }
+
+         else  if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            Debug.Log("I am pressing the left key");    
+        }
+
+           if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            Debug.Log("I am pressing the right key right now");
+        }
+
+        else if (Keyboard.current.downArrowKey.isPressed)
+        {
+            Debug.Log("I  am going backwards");
+        }
+
+
+
         transform.Rotate(0, 0, steerSpeed);
         transform.Translate(0, moveSpeed, 0);
        }
