586d976f56ba9f66fa0d83a610c9b10b342a33fd
addy <addyjaime@gmail.com>
2025-09-13 14:53:12 -0400

    adding trigger settings

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -2,8 +2,13 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-  void OnCollisionEnter2D(Collision2D collision)
-  {
+    void OnCollisionEnter2D(Collision2D collision)
+    {
         Debug.Log("oh shit you hit me homebody");
+    }
+
+  void OnTriggerEnter2D(Collider2D collision)
+  {
+        Debug.Log(" shit was that a car that just passed by, jesus");
   }
 }
